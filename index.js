f6183361b261fd487c7eec54ec61592671b40e27
Md-Rasel-Ahmed <nhd305812@gmail.com>
2025-12-26 19:09:32 +0500

    dotenv file create

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-// cafe-de-male
-// BvNDtsJzGQphXdjh
+//
+//
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  "mongodb+srv://cafe-de-male:BvNDtsJzGQphXdjh@cluster0.6plls.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.6plls.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
